d4450233f2ada83ac58c1a7ce0aefdfd6c4d433c
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-02-17 15:20:27 +0000

    Added questions arrays in index.js

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
+            
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of your project.',
+    },
+    {
+        type: 'list',
+        message: 'Select a Licence for this project!',
+        name: 'licence',
+        choices: ['MIT', 'Apache', 'GPL', 'Microsoft', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'username', // GitHub user name - to be added to Questions section include link
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input', // Email address - to be added to Questions section 
+        name: 'email', // with instructions how to reach them with additional questions.
+        message: 'What is your email address?',
+    },
 
 ];
 
