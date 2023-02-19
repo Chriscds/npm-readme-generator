c15048274154eda8d9268ad13cd13f3d962204ed
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-02-19 16:12:27 +0000

    Added all badge urls.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -30,10 +30,12 @@ function createBadge(data){
     licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
   } else if (licenseChoice === 'GPL') {
     licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }  else if (licenseChoice === 'Mozilla') {
+    licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
   } else {
     licenseBadge = ''
   } return licenseBadge;
-};// }
+};
 
 
 // function to generate markdown for README
